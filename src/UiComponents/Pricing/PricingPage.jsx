6545a6491aa13308898d38bdb5ca7160e2866b2f
biswabhusan  sahoo <biswapvt506@gmail.com>
2025-06-11 23:39:10 +0530

    All Pricing components are added and updated

--- src/UiComponents/Pricing/PricingPage.jsx
+++ src/UiComponents/Pricing/PricingPage.jsx
@@ -140,6 +140,46 @@ function PricingPage() {
           </button>
         </div>
         <GradientPaymentCard/>
+
+        {/* 4th theme */}
+        <h2 className="text-xl font-bold mt-6">Theme : SupportDev</h2>
+        <h1 className="text-2xl font-bold mb-2 mt-4">Installation</h1>
+        <h2 className="text-lg text-gray-400 mb-4">CLI</h2>
+
+        <div className="flex gap-3 mb-5">
+          <button className="bg-neutral-900 text-white px-4 py-2 rounded-md font-medium hover:bg-gray-700">
+            npm
+          </button>
+        </div>
+
+        <div className="flex items-center bg-neutral-900 p-4 rounded-lg mb-4">
+          <code className="flex-grow text-sm font-medium">
+            npx ui-vault pricing-SupportDev
+          </code>
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText("npx ui-vault pricing-SupportDev");
+              setCopied(true);
+              setTimeout(() => setCopied(false), 2000);
+            }}
+            className="text-gray-400 hover:text-white ml-2"
+          >
+            {copied ? <ClipboardCheck size={18} /> : <Clipboard size={18} />}
+          </button>
+        </div>
+
+        <div className="mt-6 flex gap-6 items-center border-b border-gray-800 pb-2 mb-4">
+          <button
+            onClick={() => setTab("preview")}
+            className={`${
+              tab === "preview" ? "text-white font-medium" : "text-gray-500"
+            }`}
+          >
+            Preview
+          </button>
+        </div>
+      
+
         <AuroraPricingFlipCards/>
 
       </div>
