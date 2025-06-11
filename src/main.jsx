3f2642cf8dc8f3672cb058a64886ba9d48beab2b
biswajit das <biswajit9348das@gmail.com>
2025-06-11 21:12:26 +0530

    listing changes

--- src/main.jsx
+++ src/main.jsx
@@ -15,10 +15,16 @@ const browserRouter = new createBrowserRouter([
   {
     path: "/components",
     element: <ComponentMainpage />,
-    children: [{
-      path: "/components/alerts",
-      element:<AlertPage/>
-    }],
+    children: [
+      {
+        path: "/components/alerts",
+        element: <AlertPage />,
+      },
+      {
+        path: "/components/card",
+        element:<LoginPage/>
+      },
+    ],
   },
 ]);
 
