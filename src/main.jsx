8a40d55ee1dd55f58c4a3a37a189d965df51db13
biswabhusan  sahoo <biswapvt506@gmail.com>
2025-06-11 17:50:25 +0530

    routing for cards is updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import ComponentMainpage from "./Components/ComponentsMainpage.jsx";
 import AlertPage from "./UiComponents/Alerts/AlertPage.jsx";
+import LoginPage from "./UiComponents/Cards/LoginPage.jsx";
 
 const browserRouter = new createBrowserRouter([
   {
@@ -14,10 +15,16 @@ const browserRouter = new createBrowserRouter([
   {
     path: "/components",
     element: <ComponentMainpage />,
-    children: [{
-      path: "/components/alerts",
-      element:<AlertPage/>
-    }],
+    children: [
+      {
+        path: "/components/alerts",
+        element: <AlertPage />,
+      },
+      {
+        path: "/components/card",
+        element:<LoginPage/>
+      },
+    ],
   },
 ]);
 
